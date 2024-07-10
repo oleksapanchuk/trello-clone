21fa32c6cc7d743649ea54683a1e65d1fd75da3b
Oleksandr Panchuk <olexandrpan4uk@gmail.com>
2024-07-11 00:30:39 +0300

    init

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+import { describe } from "node:test";
+
+export const siteConfig = {
+    name: "TaskFlow",
+    descrition: "Colaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with TaskFlow.",
+
+}
