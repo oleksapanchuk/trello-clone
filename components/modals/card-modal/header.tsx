5f21044796485585fab1b26f9ef99129cfa06b96
Oleksandr Panchuk <olexandrpan4uk@gmail.com>
2024-07-12 16:08:55 +0300

    bugs fixed

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -74,9 +74,6 @@ export const Header = ({ data }: HeaderProps) => {
                         className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
                     />
                 </form>
-                <p className="text-sm text-muted-foreground">
-                    in list <span className="underline">{data.list.title}</span>
-                </p>
             </div>
         </div>
     );
